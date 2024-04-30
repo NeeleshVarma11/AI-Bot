8e401658d9eff2a40fc7a0461b2afcd975179c7d
NeeleshVarma11 <neeleshvarma2466@gmail.com>
2024-04-30 14:39:56 +0530

    added dependencies

---
+++ src/test/java/com/example/AIBot/AiBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AIBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
