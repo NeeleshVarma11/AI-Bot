5a68af20c760892c867dad23207e7e4ad77fe8ea
NeeleshVarma11 <neeleshvarma2466@gmail.com>
2024-05-07 14:29:23 +0530

    added receiving feature by using ngrok

--- src/main/java/com/example/AIBot/Controller.java
+++ src/main/java/com/example/AIBot/Controller.java
@@ -1,10 +1,18 @@
 package com.example.AIBot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.twilio.twiml.fax.Receive.MediaType;
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.messaging.Body;
+import com.twilio.twiml.messaging.Message;
+
+import static spark.Spark.*;
 
 
 
@@ -24,5 +32,15 @@ public class Controller {
     @PostMapping
     public void sendSms(@RequestBody SmsRequest smsRequest) {
         service.sendSms(smsRequest);
+    }
+    
+    
+    @PostMapping( "/sms")
+    public String handleSms() {
+    	System.out.println("called");
+        Body body = new Body.Builder("The Robots are coming! Head for the hills!").build();
+        Message sms = new Message.Builder().body(body).build();
+        MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
+        return twiml.toXml();
     }
 }
